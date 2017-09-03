3250dd14a987b11de655383a78c53b6756f309ec
Punyugi2012 <58160216@go.buu.ac.th>
2017-09-03 19:57:46 +0700

    create example view

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,3 @@
+<nav class="navbar navbar-light bg-light">
+  <span class="h1" class="navbar-brand mb-0">โปรไฟล์</span>
+</nav>
---
+++ resources/views/profileView.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+@section('title', 'Profile')
+@section('content')
+    <div class="row" style="margin-top:20px;border:1px solid black">
+        <div class="col-md-6">
+            <img src="https://i.pinimg.com/736x/8b/6c/0d/8b6c0d68e697d49fd613f3277edef5e6--iron-man-armor-marvel-captain-america.jpg" style="max-height:600px">
+        </div>
+        <div class="cold-md-6" style="font-size:20px;margin:0px 0px 0px 80px">
+            ชื่อ: นายปุณยวีร์ เจริญท้าว<br>
+            ชื่อเล่น: แมกซ์<br>
+            อายุ: 21 ปี<br>
+            ศึกษาอยู่: คณะวิทยาการสารสนเทศ<br>
+            สาขา: วิทยาการคอมพิวเตอร์<br>
+            รหัส: 58160216<br>
+            เบอร์โทร: 0910561444<br>
+            ความสามารถ:<br>
+            <ol>
+                <li>ภาษา
+                    <ul>  
+                        <li>C++</li>
+                        <li>C</li>
+                        <li>Java</li>
+                        <li>JavaScript</li>
+                        <li>php</li>
+                        <li>Swift</li>
+                    </ul>
+                </li>
+                <li>framework
+                    <ul>  
+                        <li>Laravel 5.4</li>
+                        <li>Angular2</li>
+                        <li>Angularjs</li>
+                        <li>Codeigniter</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </li>
+            </ol>
+        </div>
+    </div>
+@endsection
